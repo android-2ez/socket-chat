a24cb9666461f43bd322a681ee90046c9f533a55
Mint <legolas.vnz@gmail.com>
2016-07-08 20:32:59 +0700

    Add @currentUsers command

--- server.js
+++ server.js
@@ -8,6 +8,33 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
+//Send message
+function sendMessage(socket, name, text, timestamp) {
+    socket.emit('message', {
+        name: name,
+        text: text,
+        timestamp: timestamp
+    });
+}
+//Send current user to provided socket
+function sendCurrentUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if(typeof info === 'undefined'){
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function(socketId){
+        var userInfo = clientInfo[socketId];
+
+        if(userInfo.room === info.room){
+            users.push(userInfo.name);
+        }
+    });
+
+    sendMessage(socket, 'System', 'Current users: ' + users.join(', '), moment.valueOf());
+}
 
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
@@ -37,10 +64,14 @@ io.on('connection', function (socket) {
 
     socket.on('message', function (message) {
         console.log('Message received: ', message.text);
-
-        message.timestamp = moment().valueOf();
-        // socket.broadcast.emit('message', message);
-        io.to(clientInfo[socket.id].room).emit('message', message);
+        if (message.text === "@currentUsers") {
+            sendCurrentUsers(socket);
+        }
+        else {
+            message.timestamp = moment().valueOf();
+            // socket.broadcast.emit('message', message);
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
     });
 
     socket.emit('message', {
