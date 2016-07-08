9f9f794505146f2b4fe2c070d199c3668029a7a0
Mint <legolas.vnz@gmail.com>
2016-07-08 13:07:29 +0700

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,9 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connect to server');
 })
+
+socket.on('message', function (message) {
+    console.log('New message:');
+    console.log(message.text);
+
+});
