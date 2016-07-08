906323ec582d28e39b17d79165730557781373d2
Mint <legolas.vnz@gmail.com>
2016-07-08 16:38:23 +0700

    Add disconnect event and message

--- server.js
+++ server.js
@@ -7,19 +7,32 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-var clientInfo ={};
+var clientInfo = {};
 
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
 
-    socket.on('joinRoom', function(req){
+    socket.on('disconnect', function () {
+        var userData = clientInfo[socket.id];
+        if (typeof userData !== 'undefined') {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+                name: 'System',
+                text: userData.name + 'has left!',
+                timestamp: moment.valueOf()
+            });
+            delete clientInfo[socket.id];
+        }
+    })
+
+    socket.on('joinRoom', function (req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
         socket.broadcast.to(req.room).emit('message', {
             name: 'System',
             text: req.name + 'has joined',
             timestamp: moment().valueOf()
-        })       
+        })
     });
 
     socket.on('message', function (message) {
@@ -33,7 +46,7 @@ io.on('connection', function (socket) {
     socket.emit('message', {
         name: 'Server',
         text: 'Welcome to the chat application!',
-        timestamp : moment().valueOf()
+        timestamp: moment().valueOf()
     });
 
 
